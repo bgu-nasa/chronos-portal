0cb6cf257e245c402c3bd9820bc1e389237d1846
Aaron Iziyaev <128286172+aaron-iz@users.noreply.github.com>
2025-12-22 19:59:30 +0200

    create calendar module (#3)

---
+++ src/modules/calendar/module.config.ts
@@ -0,0 +1,11 @@
+import type { ModuleConfig } from "@/infra";
+import React from "react";
+
+export const moduleConfig: ModuleConfig = {
+    name: "calendar",
+    owner: "noamarg@post.bgu.ac.il",
+    basePath: "/calendar",
+    song: "https://www.youtube.com/watch?v=VdQY7BusJNU",
+    routes: [],
+    navigationItems: [],
+};
