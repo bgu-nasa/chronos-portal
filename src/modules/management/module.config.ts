66ee8ddc65592cf55b5edfc5819f1c19da8d9696
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 16:07:27 +0200

    scaffold

---
+++ src/modules/management/module.config.ts
@@ -0,0 +1,10 @@
+import type { ModuleConfig } from "@/infra";
+import React from "react";
+
+export const moduleConfig: ModuleConfig = {
+    name: "management",
+    owner: "",
+    basePath: "/management",
+    routes: [],
+    navigationItems: [],
+};
