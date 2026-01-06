a19f5681a0b47dd387dde498113db45f36b12371
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 11:08:23 +0200

    create auth package

---
+++ src/modules/auth/module.config.ts
@@ -0,0 +1,10 @@
+import type { ModuleConfig } from "@/infra";
+import React from "react";
+
+export const moduleConfig: ModuleConfig = {
+    name: "Auth",
+    owner: "",
+    basePath: "/auth",
+    routes: [],
+    navigationItems: [],
+};
