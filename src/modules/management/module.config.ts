e55ef9442361012fbc9ed64a49e726256ba1045f
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 16:13:07 +0200

    sort the mess

--- src/modules/management/module.config.ts
+++ src/modules/management/module.config.ts
@@ -1,10 +1,35 @@
 import type { ModuleConfig } from "@/infra";
 import React from "react";
+import {
+    HiOutlineBriefcase,
+    HiOutlineClipboardList,
+    HiOutlineLockOpen,
+} from "react-icons/hi";
 
 export const moduleConfig: ModuleConfig = {
     name: "management",
     owner: "",
     basePath: "/management",
     routes: [],
-    navigationItems: [],
+    navigationItems: [
+        {
+            label: "Management",
+            location: "dashboard",
+            icon: React.createElement(HiOutlineBriefcase),
+            children: [
+                {
+                    label: "Departments",
+                    href: "/management/departments",
+                    location: "dashboard",
+                    icon: React.createElement(HiOutlineClipboardList),
+                },
+                {
+                    label: "Access Control",
+                    href: "/management/roles",
+                    location: "dashboard",
+                    icon: React.createElement(HiOutlineLockOpen),
+                },
+            ],
+        },
+    ],
 };
