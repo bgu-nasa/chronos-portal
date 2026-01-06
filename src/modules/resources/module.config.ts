49979152d247bd0992d3c70f48ff52a3a24ad39c
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 14:49:45 +0200

    run script

---
+++ src/modules/resources/module.config.ts
@@ -0,0 +1,10 @@
+import type { ModuleConfig } from "@/infra";
+import React from "react";
+
+export const moduleConfig: ModuleConfig = {
+    name: "Resources",
+    owner: "",
+    basePath: "/resources",
+    routes: [],
+    navigationItems: [],
+};
