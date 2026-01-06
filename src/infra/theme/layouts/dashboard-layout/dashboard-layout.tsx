214de2c8b6293525481a8ef730ffb5c15213a871
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 10:13:59 +0200

    change the layout

--- src/infra/theme/layouts/dashboard-layout/dashboard-layout.tsx
+++ src/infra/theme/layouts/dashboard-layout/dashboard-layout.tsx
@@ -1,23 +1,33 @@
 /** @author aaron-iz */
 import { Outlet } from "react-router";
+import { AppShell, Image } from "@mantine/core";
 import styles from "./dashboard-layout.module.css";
+
+function TemporaryLogo() {
+    return <Image src="/logo.png" alt="Logo" h={40} w="auto" />;
+}
 
 export default function DashboardLayout() {
     return (
-        <div className={styles.dashboardLayoutContainer}>
-            <div className={styles.dashboardHeader}>
-                <h1>DASHBOARD HEADER PLACEHOLDER</h1>
-            </div>
-
-            <div className={styles.dashboardScreen}>
-                <div className={styles.dashboardSidebar}>
-                    <h3>SIDEBAR PLACEHOLDER</h3>
+        <AppShell
+            padding="md"
+            header={{ height: 60 }}
+            navbar={{
+                width: 300,
+                breakpoint: "sm",
+            }}
+        >
+            <AppShell.Header>
+                <div className={styles.headerContainer}>
+                    <TemporaryLogo />
                 </div>
+            </AppShell.Header>
 
-                <div className={styles.dashboardContent}>
-                    <Outlet />
-                </div>
-            </div>
-        </div>
+            <AppShell.Navbar>Navbar</AppShell.Navbar>
+
+            <AppShell.Main>
+                <Outlet />
+            </AppShell.Main>
+        </AppShell>
     );
 }
