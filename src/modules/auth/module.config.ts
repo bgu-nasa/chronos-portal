6865dc22b8ed7dcf7a6b61516b075cad65a102a8
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 11:23:25 +0200

    create the login page

--- src/modules/auth/module.config.ts
+++ src/modules/auth/module.config.ts
@@ -1,10 +1,23 @@
 import type { ModuleConfig } from "@/infra";
 import React from "react";
+import { LoginPage } from "./src/pages";
 
 export const moduleConfig: ModuleConfig = {
     name: "Auth",
     owner: "",
     basePath: "/auth",
-    routes: [],
-    navigationItems: [],
+    routes: [
+        {
+            name: "login",
+            path: "/login", // Route will be /auth/login
+            element: React.createElement(LoginPage),
+        },
+    ],
+    navigationItems: [
+        {
+            label: "Login",
+            href: "/auth/login",
+            location: "public",
+        },
+    ],
 };
