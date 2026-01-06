73901a21c92bc5226c3612769837ce23dd50913f
Aaron Iziyaev <128286172+aaron-iz@users.noreply.github.com>
2026-01-06 16:14:47 +0200

    [Management] Scaffold management module and sort navs (#23)
    * scaffold
    
    * for housing others
    
    * sort the mess

--- src/modules/calendar/module.config.ts
+++ src/modules/calendar/module.config.ts
@@ -4,23 +4,23 @@ import CalendarPage from "./src/pages/CalendarPage";
 import React from "react";
 
 export const moduleConfig: ModuleConfig = {
-  name: "calendar",
-  owner: "noamarg@post.bgu.ac.il",
-  basePath: "/calendar",
-  song: "https://www.youtube.com/watch?v=VdQY7BusJNU",
-  routes: [
-    {
-      name: "calendar",
-      path: "/",
-      authorize: true,
-      element: React.createElement(CalendarPage),
-    },
-  ],
-  navigationItems: [
-    {
-      label: "Calendar",
-      href: "/calendar",
-      location: "dashboard",
-    }
-  ],
+    name: "calendar",
+    owner: "noamarg@post.bgu.ac.il",
+    basePath: "/calendar",
+    song: "https://www.youtube.com/watch?v=VdQY7BusJNU",
+    routes: [
+        {
+            name: "calendar",
+            path: "/",
+            authorize: true,
+            element: React.createElement(CalendarPage),
+        },
+    ],
+    navigationItems: [
+        // {
+        //   label: "Calendar",
+        //   href: "/calendar",
+        //   location: "dashboard",
+        // }
+    ],
 };
