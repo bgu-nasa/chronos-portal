b75e0cc9a98405c0ab52661749e68ad658e902ff
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-06 22:11:12 +0200

    testing some colors

--- src/infra/theme/mantine-theme.ts
+++ src/infra/theme/mantine-theme.ts
@@ -1,5 +1,20 @@
 /** @author aaron-iz */
 import { createTheme } from "@mantine/core";
 
-// TODO
-export const theme = createTheme({});
+export const theme = createTheme({
+    primaryColor: "brand",
+    colors: {
+        brand: [
+            "#f3f2ff",
+            "#e5e3ff",
+            "#cbc6ff",
+            "#afa7ff",
+            "#978dfe",
+            "#877cfe",
+            "#7d73fe",
+            "#6a61e3",
+            "#5e56cb",
+            "#4338ca", // rgb(67, 56, 202) - primary color
+        ],
+    },
+});
