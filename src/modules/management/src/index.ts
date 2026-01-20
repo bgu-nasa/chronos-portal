20af66e64fe0ef91457792b186ea745f97c30180
Aaron Iziyaev <aron.iz.gab@gmail.com>
2026-01-20 23:21:12 +0200

    the page

--- src/modules/management/src/index.ts
+++ src/modules/management/src/index.ts
@@ -4,3 +4,4 @@
 
 export { DepartmentsPage } from "./pages/departments-page";
 export { RolesPage } from "./pages/roles-page";
+export { OrganizationSettingsPage } from "./pages/organization-settings-page";
